72bf6dbf77e44e10179db1df5996866400e4fb08
Jürgen Galler <juergen@galler.info>
2023-10-24 20:33:43 +0200

    add ethernet support

--- main/main.c
+++ main/main.c
@@ -15,8 +15,12 @@
 #include "freertos/queue.h"
 #include "freertos/event_groups.h"
 #include "freertos/semphr.h"
+#include "sdkconfig.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "ethernet_init.h"
 #include "esp_event.h"
 #include "esp_vfs.h"
 #include "nvs_flash.h"
@@ -100,6 +104,89 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 		s_retry_num = 0;
 		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
 	}
+}
+
+/* Event handler for IP_EVENT_ETH_GOT_IP */
+static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
+                                 int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+    const esp_netif_ip_info_t *ip_info = &event->ip_info;
+
+    ESP_LOGI(TAG, "Ethernet Got IP Address");
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
+    ESP_LOGI(TAG, "IP: " IPSTR, IP2STR(&ip_info->ip));
+    ESP_LOGI(TAG, "MASK: " IPSTR, IP2STR(&ip_info->netmask));
+    ESP_LOGI(TAG, "GW: " IPSTR, IP2STR(&ip_info->gw));
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
+}
+
+void eth_init_sta(void)
+{
+    uint8_t eth_port_cnt = 0;
+    esp_eth_handle_t *eth_handles;
+    char if_key_str[10];
+    char if_desc_str[10];
+
+    // Initialize TCP/IP network interface aka the esp-netif (should be called only once in application)
+    ESP_ERROR_CHECK(esp_netif_init());
+    // Create default event loop that running in background
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // Initialize Ethernet driver
+    ESP_ERROR_CHECK(ethernet_init_all(&eth_handles, &eth_port_cnt));
+
+    // Create instance(s) of esp-netif for Ethernet(s)
+    if (eth_port_cnt == 1) {
+        // Use ESP_NETIF_DEFAULT_ETH when just one Ethernet interface is used and you don't need to modify
+        // default esp-netif configuration parameters.
+        esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
+        esp_netif_t *eth_netif = esp_netif_new(&cfg);
+        // Attach Ethernet driver to TCP/IP stack
+        ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[0])));
+    } else {
+        // Use ESP_NETIF_INHERENT_DEFAULT_ETH when multiple Ethernet interfaces are used and so you need to modify
+        // esp-netif configuration parameters for each interface (name, priority, etc.).
+        esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_ETH();
+        esp_netif_config_t cfg_spi = {
+            .base = &esp_netif_config,
+            .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH
+        };
+
+        for (int i = 0; i < eth_port_cnt; i++) {
+            sprintf(if_key_str, "ETH_%d", i);
+            sprintf(if_desc_str, "eth%d", i);
+            esp_netif_config.if_key = if_key_str;
+            esp_netif_config.if_desc = if_desc_str;
+            esp_netif_config.route_prio -= i * 5;
+            esp_netif_t *eth_netif = esp_netif_new(&cfg_spi);
+
+            // Attach Ethernet driver to TCP/IP stack
+            ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[i])));
+        }
+    }
+
+    // Register user defined event handers
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+
+    // Start Ethernet driver state machine
+    for (int i = 0; i < eth_port_cnt; i++) {
+        ESP_ERROR_CHECK(esp_eth_start(eth_handles[i]));
+    }
+
+    // Print each device info
+    for (int i = 0; i < eth_port_cnt; i++) {
+        eth_dev_info_t info = ethernet_init_get_dev_info(eth_handles[i]);
+        if (info.type == ETH_DEV_TYPE_INTERNAL_ETH) {
+            ESP_LOGI(TAG, "Device Name: %s", info.name);
+            ESP_LOGI(TAG, "Device type: ETH_DEV_TYPE_INTERNAL_ETH(%d)", info.type);
+            ESP_LOGI(TAG, "Pins: mdc: %d, mdio: %d", info.pin.eth_internal_mdc, info.pin.eth_internal_mdio);
+        } else if (info.type == ETH_DEV_TYPE_SPI) {
+            ESP_LOGI(TAG, "Device Name: %s", info.name);
+            ESP_LOGI(TAG, "Device type: ETH_DEV_TYPE_SPI(%d)", info.type);
+            ESP_LOGI(TAG, "Pins: cs: %d, intr: %d", info.pin.eth_spi_cs, info.pin.eth_spi_int);
+        }
+    }
 }
 
 bool wifi_init_sta(void)
@@ -392,12 +479,14 @@ void app_main()
 	}
 	ESP_ERROR_CHECK(ret);
 
-	// WiFi initialize
-	if (wifi_init_sta() == false) {
-		while(1) vTaskDelay(10);
-	}
-	
-	ESP_LOGI(TAG, "--- wifi init done ---");
+	eth_init_sta();
+	ESP_LOGI(TAG, "--- eth init done ---");
+
+	// // WiFi initialize
+	// if (wifi_init_sta() == false) {
+	// 	while(1) vTaskDelay(10);
+	// }
+	// ESP_LOGI(TAG, "--- wifi init done ---");
 
 	// Initialize mDNS
 	ESP_ERROR_CHECK( mdns_init() );
